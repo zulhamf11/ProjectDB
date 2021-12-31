feebfd85a2fe6a849ff7429a90703503c9498033
zulhamf11 <77193797+zulhamf11@users.noreply.github.com>
2021-12-31 22:31:58 +0700

    Add files via upload

---
+++ ProjectDB/admin/templates/sidebar.php
@@ -0,0 +1,84 @@
+<nav class="col-md-2 d-none d-md-block bg-light sidebar">
+      <div class="sidebar-sticky">
+        <ul class="nav flex-column">
+
+          <?php 
+
+
+            $uri = $_SERVER['REQUEST_URI']; 
+            $uriAr = explode("/", $uri);
+            $page = end($uriAr);
+
+          ?>
+
+
+          <li class="nav-item">
+            <a class="nav-link <?php echo ($page == '' || $page == 'index.php') ? 'active' : ''; ?>" href="index.php">
+              <span data-feather="home"></span>
+              Dashboard <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php echo ($page == 'customer_orders.php') ? 'active' : ''; ?>" href="customer_orders.php">
+              <span data-feather="file"></span>
+              Transaction
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php echo ($page == 'products.php') ? 'active' : ''; ?>" href="products.php">
+              <span data-feather="shopping-cart"></span>
+              Product Admin
+            </a>
+          </li>
+          <!-- <li class="nav-item">
+            <a class="nav-link <?php echo ($page == '#') ? 'active' : ''; ?>" href="#">
+              <span data-feather="shopping-cart"></span>
+              Product Customer
+            </a>
+          </li> -->
+          <li class="nav-item">
+            <a class="nav-link <?php echo ($page == 'brands.php') ? 'active' : ''; ?>" href="brands.php">
+              <span data-feather="shopping-cart"></span>
+              Manage Product type
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php echo ($page == 'categories.php') ? 'active' : ''; ?>" href="categories.php">
+              <span data-feather="shopping-cart"></span>
+              Categories
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?php echo ($page == 'customers.php') ? 'active' : ''; ?>" href="customers.php">
+              <span data-feather="users"></span>
+              Customers
+            </a>
+          </li>
+          <li class="nav-item">
+            <!-- <a class="nav-link <?php echo ($page == '#') ? 'active' : ''; ?>" href="#">
+              <span data-feather="users"></span>
+              Customers Request
+            </a>
+          </li> -->
+        </ul>
+
+       
+      </div>
+    </nav>
+
+
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+      <div class="d-flex just,ify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Hello, <?php echo $_SESSION["admin_name"]?> !</h1>
+        <br>
+        <div class="btn-toolbar mb-2 mb-md-0">
+          <div class="btn-group mr-2">
+            <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary">Export</button> -->
+          </div>
+          <!-- <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
+            <span data-feather="calendar"></span>
+            This week
+          </button> -->
+        </div>
+      </div>
